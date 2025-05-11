358fd04038a0eb378d617edc9da96bae66f68447
Kondratenko Mikhailo <mike.kondr@gmail.com>
2025-05-11 17:06:09 +0300

    finish

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DataGridApp
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+{    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            List<User> users = new List<User>
+            {
+                new User { Id = 1, Name = "Alice", BirthDate = new DateTime(1990, 1, 1),
+                    ImageUrl="https://www.onthisday.com/images/people/homer-simpson.jpg" },
+                new User { Id = 2, Name = "Bob", BirthDate = new DateTime(1985, 5, 15) },
+                new User { Id = 3, Name = "Charlie", BirthDate = new DateTime(2000, 10, 20) }
+            };
+
+            dg.ItemsSource = users;
         }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Details
+        {
+            get
+            {
+                return $"{Name}, дата народження {BirthDate.ToLongDateString()}, а це довгий текст опису обраного користувача.";
+            }
+        }
+        public string ImageUrl { get; set; }
     }
 }
